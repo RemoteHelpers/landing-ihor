6da81ceada3039b2e147ff9c20ab90d8d21b914b
Ihor Mural <igor201278789887000@gmail.com>
2022-08-03 11:38:35 +0300

    solution

--- src/blocks/SecondQuizPage/SecondQuizPage.tsx
+++ src/blocks/SecondQuizPage/SecondQuizPage.tsx
@@ -1,7 +1,223 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import { Link } from 'react-router-dom';
+
+import Illustration from '../../images/question1-ill.svg';
+import Logo from '../../images/Logo.svg';
+import WhatsApp from '../../images/social/whatsapp.svg';
+import Viber from '../../images/social/viber.svg';
+import Skype from '../../images/social/skype.svg';
+import Telegram from '../../images/social/telegram.svg';
+import Email from '../../images/social/email.svg';
+import Previous from '../../images/buttons/previous.svg';
+import PreviousBold from '../../images/buttons/previousBold.svg';
+import '../Buttons/Buttons.scss';
+import '../../utils/header.scss';
+import '../../utils/footer.scss';
+import '../../utils/star.scss';
+import '../../utils/notification.scss';
+import '../../utils/numberOfItem.scss';
+import '../../utils/container.scss';
+import './SecondQuizPage.scss';
 
 export const SecondQuizPage: React.FC = () => {
+  const [timer, setTimer] = useState<number>(15);
+  const [isNotification, setIsNotification] = useState<boolean>(false);
+
+  useEffect(() => {
+    if (timer > 0) {
+      const interval = setInterval(() => {
+        setTimer(timer - 1);
+      }, 1000);
+
+      return () => clearInterval(interval);
+    }
+
+    if (timer === 0) {
+      setIsNotification(true);
+    }
+
+    return undefined;
+  }, [timer]);
+
   return (
-    <h1>1</h1>
+    <div className="SecondQuizPage">
+      <div className="SecondQuizPage__content-wrapper">
+        <div className="container container__SecondQuizPage">
+          <header className="header">
+            <img
+              src={Logo}
+              alt="Logotype"
+              className="header__logo"
+            />
+            <p className="header__text header__text--first-form">
+              The outstaffing company in Digital Marketing
+            </p>
+          </header>
+
+          <main className="SecondQuizPage__main">
+            <h3 className="SecondQuizPage__title SecondQuizPage__title--desktop">
+              Our candidates work
+              {' '}
+              <span className="SecondQuizPage__title--blue">
+                8 and 4 hours
+              </span>
+              {' '}
+              a day
+            </h3>
+
+            <h3 className="SecondQuizPage__title SecondQuizPage__title--mobile">
+              Our candidates work
+              {' '}
+              <span className="SecondQuizPage__title--blue">
+                8 and 4 hours
+              </span>
+              {' '}
+              a day
+            </h3>
+
+            <div className="SecondQuizPage__buttons">
+              <Link
+                to="/incorrectFirstAnswer"
+                className="SecondQuizPage__navigate"
+              >
+                <div className="SecondQuizPage__button SecondQuizPage__button--true">False</div>
+              </Link>
+
+              <Link
+                to="/incorrectFirstAnswer"
+                className="SecondQuizPage__navigate"
+              >
+                <div className="SecondQuizPage__button SecondQuizPage__button--false">True</div>
+              </Link>
+            </div>
+
+            <img
+              src={Illustration}
+              alt="illustration"
+              className="SecondQuizPage__illustration"
+            />
+
+            <div className="numberOfItem">
+              <span className="numberOfItem--active">
+                2
+              </span>
+              /4
+            </div>
+
+            <div className="buttons SecondQuizPage__arrow">
+              <Link
+                className="buttons__previous"
+                to="/firstQuestion"
+              >
+                <img className="buttons--desktop" src={PreviousBold} alt="arrow left" />
+                Previous
+              </Link>
+            </div>
+            <div className="buttons SecondQuizPage__arrow--mobile">
+              <Link
+                className="buttons__previous"
+                to="/firstQuestion"
+              >
+                <img className="buttons--mobile" src={Previous} alt="arrow left" />
+                Previous
+              </Link>
+            </div>
+          </main>
+        </div>
+
+        <footer className="footer">
+          <ul className="footer__social">
+            <li className="footer__social-item">
+              <a
+                href="https://wa.me/380997101033"
+                className="footer__social-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={WhatsApp} alt="WhatsApp logo" className="footer__social-logo" />
+                <span>+38 099 710 10 33</span>
+              </a>
+            </li>
+            <li className="footer__social-item footer__social-item--desktop">
+              <a
+                href="viber://chat?number=972509014509"
+                className="footer__social-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Viber} alt="Viber logo" className="footer__social-logo" />
+                <span>+97 250 901 45 09</span>
+              </a>
+            </li>
+            <li className="footer__social-item footer__social-item--desktop">
+              <a
+                href="skype:youisrael?chat"
+                className="footer__social-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Skype} alt="Skype logo" className="footer__social-logo" />
+                <span>youisrael</span>
+              </a>
+            </li>
+            <li className="footer__social-item">
+              <a
+                href="https://telegram.me/nikokardc"
+                className="footer__social-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Telegram} alt="Telegram logo" className="footer__social-logo" />
+                <span>@nikokardc</span>
+              </a>
+            </li>
+            <li className="footer__social-item">
+              <a
+                href="mailto:sales@rh-s.com"
+                className="footer__social-link"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={Email} alt="Email logo" className="footer__social-logo" />
+                <span>sales@rh-s.com</span>
+              </a>
+            </li>
+          </ul>
+        </footer>
+      </div>
+
+      <div className="SecondQuizPage__info">
+        <header className="header--desktop">
+          <img
+            src={Logo}
+            alt="Logotype"
+            className="header__logo"
+          />
+          <p className="header__text">
+            The outstaffing company
+            <br />
+            in Digital Marketing
+          </p>
+        </header>
+
+        <footer>
+          <button type="button" className="footer__text">
+            I&apos;m curious, want to get a consultation
+          </button>
+        </footer>
+      </div>
+
+      {!isNotification && (
+        <div className="star">
+          <span className="star__text">{timer}</span>
+        </div>
+      )}
+
+      {isNotification && (
+        <div className="notification">
+          Please click on your answer
+        </div>
+      )}
+    </div>
   );
 };
